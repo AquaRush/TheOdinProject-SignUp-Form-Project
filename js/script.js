531a85195e6070811c86712dfa6f0aefcea61530
AquaRush <george.mihaylov96@gmail.com>
2024-04-09 13:07:02 +0300

    Update script.js
    Modified the script to reduce the `if/else` usage using return early pattern.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ function passwordValidator() {
     const password = document.getElementById('grid-password').value;
     const confirmPassword = document.getElementById('grid-confirm-password').value;
 
-    if (password === confirmPassword) {
-        return true;
-    } else {
+    if (password !== confirmPassword) {
         alert("Password does not match.");
         return false;
     }
+
+    return true;
 }
